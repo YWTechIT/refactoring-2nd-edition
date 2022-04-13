7a39f5d3686c909b3ddc5d87bcb8af7fb7da59c5
YWTechIT <abcd8637@naver.com>
2022-04-13 09:59:21 +0900

    변수 인라인 하기: totalVolumeCredits를 임시 변수로 선언하여 적용하는게 아니라 곧바로 적용함

--- 1-refactoring-first-example/refactor-statement.js
+++ 1-refactoring-first-example/refactor-statement.js
@@ -10,9 +10,8 @@ function refactorStatement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = totalVolumeCredits();
   result += `총액: ${usd(totalAmount)}`;
-  result += `적립 포인트: ${volumeCredits}점 \n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점 \n`;
   return result;
 
   function totalVolumeCredits() {
