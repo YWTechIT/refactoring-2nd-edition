49552512af8f8d1af717b11f4becdb6c41119edb
YWTechIT <abcd8637@naver.com>
2022-04-13 09:33:20 +0900

    Add playFor function: 임시 변수를 질의 함수로 바꾸기, 매개변수로 전달할 필요 없이 amountFor()안에서 다시 계산한다.

--- 1-refactoring-first-example/refactor-statement.js
+++ 1-refactoring-first-example/refactor-statement.js
@@ -9,7 +9,7 @@ function refactorStatement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -24,6 +24,10 @@ function refactorStatement(invoice, plays) {
   result += `총액: ${format(totalAmount / 100)}`;
   result += `적립 포인트: ${volumeCredits}점 \n`;
   return result;
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 }
 
 function amountFor(aPerformance, play) {
@@ -48,7 +52,6 @@ function amountFor(aPerformance, play) {
   }
 
   return result;
-  0;
 }
 
 export default refactorStatement;
